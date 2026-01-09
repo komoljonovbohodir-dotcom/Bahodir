0fae8e7e6791b931d8b6c519b1272168374e39b9
komoljonovbohodir-dotcom <komoljonovbohodir@gmail.com>
2026-01-09 15:00:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
-import sys,os bahodir2010
+import sys,os 
+# bahodir2010
 
 from aiogram import Bot, Dispatcher, html, F
 from aiogram.client.default import DefaultBotProperties
@@ -94,6 +95,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
     asyncio.run(main())
+
 
 
 
